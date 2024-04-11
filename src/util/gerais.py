940e82c4f445d3d14ba12790045761190efd3125
trdntb <130498186+trdntb@users.noreply.github.com>
2024-04-11 01:16:53 -0300

    jogador modificado
    .

---
+++ src/util/gerais.py
@@ -0,0 +1,12 @@
+def mostrar_objeto(indice, objeto) -> None:
+    separador = '-'
+    ordem = indice + 1
+    frase = f'{ordem} {separador} {objeto}'
+    print(frase)
+
+
+def mostrar_objetos(cabecalho, lista, filtros=None) -> None:
+    if filtros is not None: print(filtros)
+    print(cabecalho)
+    for indice, objeto in enumerate(lista):
+        mostrar_objeto(indice, str(objeto))
